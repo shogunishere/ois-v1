c4066092d734e5a6fe2d252080a8d0b9157fe534
shogunishere <bc9389@student.uni-lj.si>
2020-02-28 17:27:02 +0000

    #2 in dodana slika

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,8 +1,14 @@
-import java.lang.Math; 
+import java.lang.Math;
+import java.util.Scanner;
 
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
+        System.out.print("nadmorska visina: ");
+        Scanner sc = new Scanner(System.in);
+        int visina = sc.nextInt();
+        
+        izpis(visina);
     }
 
     public static void izpis(int visina) {
@@ -10,10 +16,10 @@ public class Gravitacija {
         double M = 5.972 * Math.pow(10, 24);
         double r = 6.371 * Math.pow(10, 6);
 
-        double a = (C * M) / (Math.pow(r + v,2));
+        double a = (C * M) / (Math.pow(r + visina,2));
 
-        System.out.println("nadmorska visina: " + v);
-        System.out.println("gravitacijski pospesek: " + v);
+        System.out.println("nadmorska visina: " + visina + " m");
+        System.out.println("gravitacijski pospesek: " + a + " m/s^2");
     }
 
     public static double pospesekNaVisini(int v) {
