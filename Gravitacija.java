ecfb47f621beac740a497593dbaa18bac71f9b60
shoguishere <colnar.brin3@gmail.com>
2020-02-28 17:38:24 +0100

    #1 in #4

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,19 @@
+import java.lang.Math; 
+
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
+        izpis(100);
+    }
+
+    public static void izpis(int v) {
+        double C = 6.674 * Math.pow(10, -11);
+        double M = 5.972 * Math.pow(10, 24);
+        double r = 6.371 * Math.pow(10, 6);
+
+        double a = (C * M) / (Math.pow(r + v,2));
+
+        System.out.println("nadmorska visina: " + v);
+        System.out.println("gravitacijski pospesek: " + v);
     }
 }
